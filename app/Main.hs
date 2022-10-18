e0d7b07988084d1d8b66b083b6590edbfe4790f8
airi <airirarux@gmail.com>
2022-10-18 22:07:06 +0900

    1017

--- app/Main.hs
+++ app/Main.hs
@@ -11,30 +11,25 @@ main = interact func
 
 func :: String -> String
 
--- func :: String -> String
--- func input = unlines ["Hello Algo-method!"]
-
--- func :: String -> String
--- func input = unlines (map showInt [1 .. 3])
-
--- func :: String ->String
--- func input = showInt 27182 `mad` 818
-
--- func :: String -> String
--- func input = showInt (314 * (159 + 265) - 358)
-
--- func :: String -> String
+-- func input = unlines [show (27182 `mod` 818)]
+-- func input = unlines [show (314 * (159 + 265) - 358)]
 -- func input = showInt (24 * 60 * 60)
 
--- func input = showInt (2 * readInt input)
--- func showInt . (2 *) . readInt
+-- func input = case map (map readInt . words) (lines input) of
+--     [n]:_ -> unlines [show (2 * n)]
+--     _     -> error "invalid input"
 
--- func input =  case readInt input of
---     n -> showInt (2 * n)
+-- func input = case map (map readInt . words) (lines input) of
+--     [n]:_ -> unlines [show (n `mod` 5)]
+--      _     -> error "invalid input"
 
--- func input = case readInt input of
---     n -> showInt (n `mod` 5)
+-- func input case map words (lines input) of
+--     [s]:_ -> unlines [concat (replicate 3 s)]
+--     _     -> error "invalid input"
 
-func input = case words input of
-    s:_ -> unlines [concat (replicate 3 s)]
-    [] -> error "invalid input"
+-- func input case map words (lines input) of
+--     [[s]] -> unlines [s !! 2]
+
+func input = case map (map readInt . words) (lines input) of
+    [[x]] -> unlines [showInt (24 - x)]
+    
--- src/TemplateAlgo.hs
+++ src/TemplateAlgo.hs
@@ -13,5 +13,3 @@ func :: String -> String
 func input = case map (map readInt . words) (lines input) of
     _ -> undefined
 
-readInt :: String -> Int
-readInt =read
